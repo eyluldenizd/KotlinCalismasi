cd750363bbde9957efc92573a9e261a4179e3007
eyluldenizd <158774187+eyluldenizd@users.noreply.github.com>
2025-04-25 22:58:32 +0300

    Add files via upload
    1. ÖDEV

---
+++ variables.kt
@@ -0,0 +1,67 @@
+﻿package com.example.kotlinders.degiskenler
+
+fun main() {
+    // Kişisel Bilgiler
+    val sehir = "Adana"
+    val ulke = "Türkiye"
+    val telefon = "+90 530 123 4567"
+    val postaKodu = "00170"
+    val email = "mail@gmail.com"
+    val meslek = "Bilgisayar Mühendisi"
+    val dogumGunu = "30/07/2003"
+    var medeniDurum = "Bekar"
+
+    // Finans
+    val maas = 30000.0
+    var bakiye = 1345.75
+    val ödeme = 299.99
+    val ödemeTarihi = "04/04/2025"
+    val indirimMiktarı = 50.0
+    val yemekFiyatı = 120.0
+
+    // Sipariş
+    val müşteriAdı = "Eylül Deniz"
+    val siparişAdeti = 1
+    val ürünAdı = "Bluetooth Kulaklık"
+    val ürünYorum = "Çok memnun kaldım, ses kalitesi harika."
+    val ürünPuanı = 4.8
+
+    // Araba / Telefon
+    val arabaModeli = "Toyota Corolla"
+    val telefonModeli = "Samsung Galaxy S24+"
+    val ekranBoyutu = 6.7
+    val ağırlık = 185.0
+
+    // Kitap / Medya
+    val kitapAdı = "Kotlin Programlama"
+    val yayınlamaTarihi = "01/01/2015"
+    val müzikAdı = "Shake it Off"
+    val videoSüresi = "2:35"
+    val resimAdı = "picture.jpg"
+    val dosyaFormatı = "jpg"
+
+    // Adres
+    val enlem = 41.0082
+    val boylam = 28.9784
+    val faturaAdresi = "Huzurevleri Mah. No:123 Çukurova/Adana"
+    val sokakAdı = "Atatürk Caddesi"
+
+    // Renk
+    val renk = "Mavi"
+    val renkKodu = "#0000FF"
+
+    // Diğer
+    val odaSayısı = 2
+    val marka = "Apple"
+    val ulusalGün = "23 Nisan"
+    val tatilGünü = "1 Ocak"
+    val rezervasyonTarihi = "30/09/2025"
+    val otobüsHattı = "A123"
+    var kalanDakika = 12
+    val takipKodu = "TR123456789"
+    var kuponSüresi = "5 gün"
+    val kuponKodu = "INDIRIM50"
+
+    // Konsola örnek bir çıktı
+    println("Müşteri: $müşteriAdı, Ürün: $ürünAdı, Fiyat: $ödeme TL, Kalan Bakiye: $bakiye TL")
+}
